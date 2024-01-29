f06a6b53c7595b14787239ffdfd356ac59de4001
matthewhelan <36001451+matthewhelan@users.noreply.github.com>
2024-01-29 16:07:34 +0000

    l1

--- Student/lecture_03_04.lean
+++ Student/lecture_03_04.lean
@@ -191,6 +191,39 @@ be returned.
 #eval id_poly'' String "Hello!"   -- error
 #eval id_poly'' Bool true         -- error
 
+
+-- IN Class work
+
+def comp' (α β γ : Type) : (β → γ) → (α → β) → (α → γ)
+| g, f => fun a => g (f a)
+
+def square (n : Nat) : Nat := n^2
+def inc (n : Nat) : Nat := Nat.succ n
+
+#reduce (comp' Nat Nat Nat square inc) 5
+
+def is_even (n : Nat) : Bool := n%2 = 0
+#check String.length
+
+def is_even_len : String → Bool := comp' String Nat Bool is_even String.length
+#reduce is_even_len "Hellof"
+
+
+def comp {α β γ : Type} : (β → γ) → (α → β) → (α → γ)
+| g, f => fun a => g (f a)
+
+def is_even_len' := comp is_even String.length
+def is_even_len'' := is_even ∘ String.length
+
+def apply_to {α : Type} : (α → α) → (α → α)
+| f => fun a => f (f a)
+
+-- Want to not allow for two arguments (function and its argument) and instead
+-- just a function applied to an argument twice
+
+#reduce apply_to inc 5
+
+
 /-!
 Sometimes we will have to give type arguments
 explicitly, even when they're declared to be
